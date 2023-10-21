4ddaf952321445c78cb7a187b7592aed42c3db17
Panatchakorn <panatchakorn@gmail.com>
2023-10-21 15:25:06 +1300

    add message

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const github = require('@actions/github');
     async() => {
         try{
             core.notice("Calling our action");
+            core.message("Calling my action");
         }
+        
         catch(error){
             core.setFailed(error.message);
         }
