4ba92c905f40d47b1b9cd3e00a567d45164c95d0
panatchakorn <panatchakorn@gmail.com>
2023-10-25 15:37:29 +1300

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,36 @@ async function checkFileStartsWithHeader(filePath){
             checkFileExists("LICENSE");
             if (
                 ! await checkFileStartsWithHeader("REAMME.md")
-            ){}
+            ){
+                //get token for octokit
+                const token = core.getInput('repo-token');
+                const octokit = new github.getOctokit(token);
+
+                // call octokit to create a check with annotation and details
+                const check = await octokit.checks.create({
+                    owner: github.context.repo.owner,
+                    repo: github.context.repo.repo,
+                    name: 'readme-header',
+                    head_sha: github.context.sha,
+                    status: 'completed',
+                    conclusion: 'failure',
+                    output:{
+                        title: 'README.md does not start with a header',
+                        summary: 'README.md does not start with a header',
+                        annotations:[
+                            {
+                                path: 'README.md',
+                                start_line: 1,
+                                end_line: 1,
+                                annotation_level: 'failure',
+                                message: 'READEME.md does not start with a header'
+                                start_column:1,
+                                end_column:1
+                            }
+                        ]
+                    }
+                });
+            }
         }
         
         catch(error){
