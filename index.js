01a082c6b6dc5dda7e2530e7ccbd0f0820103f05
panatchakorn <panatchakorn@gmail.com>
2023-10-25 15:30:09 +1300

    Update index.js

--- index.js
+++ index.js
@@ -14,11 +14,29 @@ async function checkFileExists(filePath) {
     });
 }
 
+async function checkFileStartsWithHeader(filePath){
+    return fs.promises.readFile(filePath,'utf8')
+    .then(fileContent => {
+        // remove all spaces at the beginning of the file
+        fileContent = fileContent.replace( ^\s*\n gm, '');
+        if (fileContent.startWith('#')){
+            core.info('File ${filePath} starts with a header');
+            return true;            
+        } else{
+          core.setFailed('File ${filePath}  does not start with a header' );
+            return false;
+        }
+    });
+}
+
 (  async() => {
         try{
             core.notice("Calling our action");
             checkFileExists("README.md");
             checkFileExists("LICENSE");
+            if (
+                ! await checkFileStartsWithHeader("REAMME.md")
+            ){}
         }
         
         catch(error){
